1003a6f96e8ad2362528fcc4b35c12af3533ab38
Lailson Sobral <lailsonlm@hotmail.com>
2023-05-16 22:12:12 -0300

    create home page on web and mobile and first routes on backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true, // todas as URLs de front podem acessar o backend
+  // origin: ['http://localhost:3000', 'https://meusite.com.br'],
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
